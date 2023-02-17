b2bedcc2378a7e2836842a994fc2b691bda66224
mhmd-fl <mohammed@founderandlightning.com>
2023-02-17 19:55:21 +0530

    Initial commit

---
+++ src/layout/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="nk-footer">
+      <div className="container-fluid">
+        <div className="nk-footer-wrap">
+          <div className="nk-footer-copyright">
+            {" "}
+            &copy; 2023 BYI. Designed and Developed by <a href="https://buildyourinnovation.com">Build Your Innovation</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Footer;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import Head from "../layout/head/Head";
+import Content from "../layout/content/Content";
+import { Row, Col } from "reactstrap";
+import classNames from "classnames";
+import {
+  Block,
+  PreviewCard,
+  Button,
+} from "../components/Component";
+import axios from "axios";
+
+const Homepage = ({ headColor, striped, border, hover, responsive }) => {
+  const [businessKeywords, setBusinessKeywords] = useState([]);
+  const [clientKeywords, setClientKeywords] = useState([]);
+  const [responseData, setResponseData] = useState();
+  const [record, setRecord] = useState(null);
+  const [formData, setFormData] = useState({
+    businessKeyword: '',
+    clientKeyword: [],
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleChangeBusiness = (event) => {
+    event.preventDefault();
+    setBusinessKeywords((prevState) => {
+      return [...prevState, formData.businessKeyword];
+    });
+    setFormData({ businessKeyword: '' });
+  };
+
+  const handleChangeClient = (event) => {
+    event.preventDefault();
+    setClientKeywords((prevState) => [...prevState, formData.clientKeyword]);
+    setFormData({ clientKeyword: [] });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('this is business keywords', businessKeywords[0].toString());
+    console.log('this is client keywords', clientKeywords);
+    const data = JSON.stringify({
+      query: `mutation($businessKeyword: String!, $clientKeyword: [String!]!) {
+              createConnection(input: {
+                  businessKeyword: $businessKeyword
+                  clientKeyword: $clientKeyword
+              }) {
+                  subject
+                  body
+              }
+          }`,
+      variables: {
+        businessKeyword: businessKeywords[0],
+        clientKeyword: clientKeywords,
+      },
+    });
+
+    const config = {
+      method: 'post',
+      url: 'https://starfish-app-fzf2t.ondigitalocean.app/graphql',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log('line 63', response.data.data.createConnection);
+        setResponseData(response.data.data.createConnection);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setFormData({ businessKeyword: '', clientKeyword: [] });
+  };
+
+  const tableClass = classNames({
+    table: true,
+    "table-bordered": border,
+    "table-borderless": !border,
+    "table-striped": striped,
+    "table-hover": hover,
+  });
+  return (
+    <React.Fragment>
+      <Head title="Form Layouts" />
+      <Content page="component">
+        <Block size="lg">
+          <Row className="g-gs">
+            <Col lg="3"></Col>
+            <Col lg="6">
+              <PreviewCard className="h-100">
+                <div className="card-head">
+                  <h5 className="card-title">Generate Your AI Response</h5>
+                </div>
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label className="form-label" htmlFor="cf-full-name">
+                      Business Keyword
+                    </label>
+                    <br />
+                    {businessKeywords.map((keyword) => (
+                      <span style={{paddingRight: 10 + 'px'}}>{keyword}</span>
+                    ))}
+                    <input type="text" id="businessKeyword" name="businessKeyword" value={formData.businessKeyword} className="form-control" onChange={handleChange} />
+                    <br />
+                    <button class="btn-round btn btn-primary btn-sm" onClick={handleChangeBusiness}>Add Business Keyword</button>
+                  </div>
+                  <div className="form-group">
+                    <label className="form-label" htmlFor="cf-email-address">
+                      Client Keywords
+                    </label>
+                    <br />
+                    {clientKeywords.map((keyword) => (
+                      <span style={{paddingRight: 10 + 'px'}}>{keyword}</span>
+                    ))}
+                    <input type="text" id="clientKeyword" name="clientKeyword" value={formData.clientKeyword} className="form-control" onChange={handleChange} />
+                    <br />
+                    <button class="btn-round btn btn-primary btn-sm" onClick={handleChangeClient}>Add Client Keyword</button>
+                  </div>
+                  <div className="form-group">
+                    <Button color="primary" type="submit" size="lg">
+                      Generate
+                    </Button>
+                  </div>
+                </form>
+              </PreviewCard>
+            </Col>
+            <Col lg="3"></Col>
+          </Row>
+        </Block>
+      </Content>
+      {responseData !== undefined && 
+        <Content>
+        <Block>
+          <Row>
+            <Block size="lg">
+              <PreviewCard>
+                  <div className={responsive ? "table-responsive" : ""}>
+                    <table className={tableClass}>
+                    <thead className={`${headColor ? `table-${headColor}` : ""}`}>
+                      <tr>
+                        <td>#</td>
+                        <td>Subject</td>
+                        <td>Body</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {responseData.map((item, index) => {
+                        return (
+                          <tr key={index+1}>
+                            <th>{index+1}</th>
+                            <td>{item.subject}</td>
+                            <td>{item.body}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              </PreviewCard>
+            </Block>
+          </Row>
+        </Block>
+      </Content>
+      }
+    </React.Fragment>
+  );
+};
+export default Homepage;
