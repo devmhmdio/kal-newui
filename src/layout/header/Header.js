a32dac9e5cd430c489fd1ec4222fd4d638a7ded7
mhmd-fl <mohammed@founderandlightning.com>
2023-02-17 23:32:01 +0530

    Added deleteAllResponses button

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -5,6 +5,7 @@ import Logo from "../logo/Logo";
 import News from "../news/News";
 import User from "./dropdown/user/User";
 import Notification from "./dropdown/notification/Notification";
+import axios from "axios";
 
 const Header = ({ fixed, theme, className, setVisibility, ...props }) => {
   const headerClass = classNames({
@@ -14,6 +15,40 @@ const Header = ({ fixed, theme, className, setVisibility, ...props }) => {
     [`is-${theme}`]: theme !== "white" && theme !== "light",
     [`${className}`]: className,
   });
+
+  const deletePreviousResponses = async () => {
+    const data = JSON.stringify({
+      query: `mutation {
+        deleteAllResponsesFromDB {
+                  status {
+                    code
+                    header
+                    description
+                    moreInfo
+                  }
+              }
+          }`,
+    });
+
+    const config = {
+      method: 'post',
+      url: 'https://starfish-app-fzf2t.ondigitalocean.app/graphql',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log('line 63', response.data.data.deleteAllResponsesFromDB);
+        alert('All previous responses are deleted successfully')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className={headerClass}>
       <div className="container-fluid">
@@ -38,6 +73,9 @@ const Header = ({ fixed, theme, className, setVisibility, ...props }) => {
               </li>
               <li className="notification-dropdown me-n1"  onClick={() => setVisibility(false)}>
                 <Notification />
+              </li>
+              <li>
+                <button className="btn-round btn btn-primary btn-sm" onClick={deletePreviousResponses}>Clear All Responses</button>
               </li>
             </ul>
           </div>
