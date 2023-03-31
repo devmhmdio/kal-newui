e4758face3501a4f10cd8f4eb5ea2c34e63a9486
mhmd-fl <mohammed@founderandlightning.com>
2023-04-01 01:11:38 +0530

    Hiding options from menu

--- src/layout/menu/MenuData.js
+++ src/layout/menu/MenuData.js
@@ -1,48 +1,48 @@
 const menu = [
   {
     icon: "dashlite",
-    text: "Start Here",
+    text: "AI Training",
     link: "/",
   },
   {
     icon: "upload-cloud",
-    text: "One To One Email Campaign",
+    text: "Email Campaign",
     link: "/one-to-one",
   },
-  {
-    icon: "table-view",
-    text: "One To Many Email Campaign",
-    link: "/one-to-many",
-  },
+  // {
+  //   icon: "table-view",
+  //   text: "One To Many Email Campaign",
+  //   link: "/one-to-many",
+  // },
   {
     icon: "emails",
     text: "Send Emails",
     link: "/send-email",
   },
-  {
-    icon: "update",
-    text: "Update Prompt",
-    link: "/update-prompt",
-  },
+  // {
+  //   icon: "update",
+  //   text: "Update Prompt",
+  //   link: "/update-prompt",
+  // },
   {
     icon: "upload-cloud",
     text: "One To One SMS Campaign",
     link: "/one-to-one-message",
   },
-  {
-    icon: "table-view",
-    text: "One To Many SMS Campaign",
-    link: "/one-to-many-message",
-  },
+  // {
+  //   icon: "table-view",
+  //   text: "One To Many SMS Campaign",
+  //   link: "/one-to-many-message",
+  // },
   {
     icon: "msg",
     text: "Send SMS",
     link: "/send-message",
   },
-  {
-    icon: "update",
-    text: "Update SMS Prompt",
-    link: "/update-message-prompt",
-  }
+  // {
+  //   icon: "update",
+  //   text: "Update SMS Prompt",
+  //   link: "/update-message-prompt",
+  // }
 ];
 export default menu;
