c7d4c5f13b2ea09f0fa9ad69691bf3e0b3ce0958
mhmd-fl <mohammed@founderandlightning.com>
2023-04-03 22:52:41 +0530

    Reordering menu

--- src/layout/menu/MenuData.js
+++ src/layout/menu/MenuData.js
@@ -5,40 +5,41 @@ const menu = [
     link: "/",
   },
   {
+    icon: "emails",
+    text: "Send Emails",
+    link: "/send-email",
+  },
+  {
     icon: "upload-cloud",
     text: "Email Campaign",
     link: "/one-to-one",
+  },
+  {
+    icon: "msg",
+    text: "Send SMS",
+    link: "/send-message",
+  },
+  {
+    icon: "upload-cloud",
+    text: "SMS Campaign",
+    link: "/one-to-one-message",
   },
   // {
   //   icon: "table-view",
   //   text: "One To Many Email Campaign",
   //   link: "/one-to-many",
   // },
-  {
-    icon: "emails",
-    text: "Send Emails",
-    link: "/send-email",
-  },
   // {
   //   icon: "update",
   //   text: "Update Prompt",
   //   link: "/update-prompt",
   // },
-  {
-    icon: "upload-cloud",
-    text: "One To One SMS Campaign",
-    link: "/one-to-one-message",
-  },
   // {
   //   icon: "table-view",
   //   text: "One To Many SMS Campaign",
   //   link: "/one-to-many-message",
   // },
-  {
-    icon: "msg",
-    text: "Send SMS",
-    link: "/send-message",
-  },
+  
   // {
   //   icon: "update",
   //   text: "Update SMS Prompt",
