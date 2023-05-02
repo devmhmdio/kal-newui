a59a253da3ecf8be30f5cc25d2618dc82821f47f
mhmd-fl <mohammed@founderandlightning.com>
2023-05-02 19:38:38 +0530

    Added payments

--- src/pages/pre-built/projects/ProjectCard.js
+++ src/pages/pre-built/projects/ProjectCard.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "../../../layout/head/Head";
 import Content from "../../../layout/content/Content";
+import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import {
   Block,
   BlockHead,
@@ -12,22 +14,84 @@ import {
   ProjectCard,
   Col,
 } from "../../../components/Component";
+import axios from "axios";
+import { axiosConfig } from "../../../utils/Utils";
+
+const stripePromise = loadStripe("pk_test_vtUu95bAR4Ki4kzmIYBdaC3A00yharGIkO");
+
+const CheckoutForm = ({ email, onPayment }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    const result = await stripe.createToken(card);
+
+    if (result.error) {
+      console.log('line 37',result.error.message);
+    } else {
+      console.log('line 39',result.token);
+      onPayment(5); // Pass the amount you want to charge here
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <div style={{marginTop: 15 + "px" }}></div>
+      <Button color="primary" type="submit" size="md" disabled={!stripe}>
+        Make Payment
+      </Button>
+    </form>
+  );
+};
 
 const ProjectCardPage = () => {
+  const [token, setToken] = useState(localStorage.getItem("accessToken"));
+  const [loggedInEmail, setLoggedInEmail] = useState("");
   const [formData, setFormData] = useState({
-    title: "",
-    subtitle: "",
-    description: "",
-    lead: "",
-    tasks: 0,
-    team: [],
-    totalTask: 0,
-    date: new Date(),
+    customAmount: "",
   });
+
+  useEffect(() => {
+    const dataToken = JSON.stringify({
+      query: `mutation($token: String) {
+                  returnToken(token: $token)
+            }`,
+      variables: {
+        token,
+      },
+    });
+
+    axios(axiosConfig(dataToken))
+      .then((response) => setLoggedInEmail(response.data.data.returnToken.email))
+      .catch((error) => console.log("line 52", error));
+  }, []);
 
   // OnChange function to get the input data
   const onInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onPayment = (amount) => {
+    const data = JSON.stringify({
+      query: `mutation($amount: Int!, $email: String!) {
+          capturePayment(amount: $amount, email: $email)
+        }`,
+      variables: {
+        amount,
+        email: loggedInEmail,
+      },
+    });
+
+    axios(axiosConfig(data))
+      .then((res) => console.log("this is res", res))
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -44,55 +108,27 @@ const ProjectCardPage = () => {
 
         <Block>
           <Row className="g-gs">
-            <Col sm="6" lg="3" xxl="3">
+            <Col sm="12" lg="6" xxl="6">
               <ProjectCard>
                 <div className="project-head">
                   <div className="project-info">
-                    <h6 className="title">USD 5</h6>
-                    <span className="sub-text">Recharge your balance to USD 5 for generating emails and messages</span>
-                    <Button color="primary" type="submit" size="md">Make Payment</Button>
+                    <h6 className="title">Recharge for USD 5</h6>
+                    <span className="sub-text" style={{marginBottom: 25 + "px" }}>
+                      Recharge your balance to USD 5 for generating emails and messages
+                   
+                      </span>
+                      <Elements stripe={stripePromise}>
+                        <CheckoutForm email={loggedInEmail} onPayment={onPayment} />
+                      </Elements>
+                    </div>
                   </div>
-                </div>
-              </ProjectCard>
-            </Col>
-            <Col sm="6" lg="3" xxl="3">
-              <ProjectCard>
-                <div className="project-head">
-                  <div className="project-info">
-                    <h6 className="title">USD 10</h6>
-                    <span className="sub-text">Recharge your balance to USD 10 for generating emails and messages</span>
-                    <Button color="primary" type="submit" size="md">Make Payment</Button>
-                  </div>
-                </div>
-              </ProjectCard>
-            </Col>
-            <Col sm="6" lg="3" xxl="3">
-              <ProjectCard>
-                <div className="project-head">
-                  <div className="project-info">
-                    <h6 className="title">USD 15</h6>
-                    <span className="sub-text">Recharge your balance to USD 15 for generating emails and messages</span>
-                    <Button color="primary" type="submit" size="md">Make Payment</Button>
-                  </div>
-                </div>
-              </ProjectCard>
-            </Col>
-            <Col sm="6" lg="3" xxl="3">
-              <ProjectCard>
-                <div className="project-head">
-                  <div className="project-info">
-                    <h6 className="title">Recharge Custom Amount</h6>
-                    <span className="sub-text">Minimum Amount USD 5</span>
-                    <input type="text" className="form-control" placeholder="20"/>
-                    <Button color="primary" type="submit" size="md">Make Payment</Button>
-                  </div>
-                </div>
-              </ProjectCard>
-            </Col>
-          </Row>
-        </Block>
-      </Content>
-    </React.Fragment>
-  );
-};
-export default ProjectCardPage;
+                </ProjectCard>
+              </Col>
+            </Row>
+          </Block>
+        </Content>
+      </React.Fragment>
+    );
+  };
+  export default ProjectCardPage;
+  
